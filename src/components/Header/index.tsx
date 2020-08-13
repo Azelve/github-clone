1de122de0821e613bf9be6e1c286b743c51084a7
Rodrigo Camilo <43912333+Azelve@users.noreply.github.com>
2020-08-13 14:45:44 -0300

    finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-// import { Container } from './styles';
+import { Container, GithubLogo, SearchForm } from "./styles";
 
-const Header: React.FC = () => {
-  return <div />;
+import { ThemeName } from "../../styles/themes";
+
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newname: ThemeName) => void;
 }
+
+const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate("/" + search.toLowerCase().trim());
+  }
+
+  function toggleTheme() {
+    setThemeName(themeName === "light" ? "dark" : "light");
+  }
+
+  return (
+    <Container>
+      <GithubLogo onClick={toggleTheme} />
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter Username or Repo..."
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+      </SearchForm>
+    </Container>
+  );
+};
 
 export default Header;
---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
+
+const Repo: React.FC = () => {
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={`/Azelve`}>
+          Azelve
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"/Azelve-Bin2Dec-mobile"}>
+          youtube-content
+        </Link>
+      </Breadcrumb>
+
+      <p>Contains all of my YouTube lessons code.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>2</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton
+        target="_blank"
+        href={"https://github.com/Azelve/Bin2Dec-mobile"}
+      >
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
+};
+
+export default Repo;
